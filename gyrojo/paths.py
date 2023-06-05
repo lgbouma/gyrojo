e170a104388f88655122817d22034e887d79ab01
lgbouma <bouma.luke@gmail.com>
2023-06-05 13:03:47 -0700

    update paths

--- gyrojo/paths.py
+++ gyrojo/paths.py
@@ -4,17 +4,22 @@ it makes a hidden local cache directory, at ~/.gyrointerp_cache, where results
 from large batch runs will by default be stored.
 """
 import os
+from os.path import join
 from gyrojo import __path__
 __path__ = list(__path__)
 
-DATADIR = os.path.join(__path__[0], 'data')
-RESULTSDIR = os.path.join(os.path.dirname(__path__[0]), 'results')
-CACHEDIR = os.path.join(os.path.expanduser('~'), '.gyrojo_cache')
+DATADIR = join(__path__[0], 'data')
 
-for l in [DATADIR, CACHEDIR, RESULTSDIR]:
+RESULTSDIR = join(os.path.dirname(__path__[0]), 'results')
+
+TABLEDIR = join(RESULTSDIR, 'tables')
+
+CACHEDIR = join(os.path.expanduser('~'), '.gyrojo_cache')
+
+for l in [DATADIR, CACHEDIR, RESULTSDIR, TABLEDIR]:
     if not os.path.exists(l):
         print(f"Making {l}")
         os.mkdir(l)
 
 # used for making the manuscript's plots, and not any reusable functionality
-LOCALDIR = os.path.join(os.path.expanduser('~'), 'local')
+LOCALDIR = join(os.path.expanduser('~'), 'local')
