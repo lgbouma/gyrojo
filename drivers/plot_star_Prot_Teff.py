978d5267829728d57f2c405fac707b5b01da5d87
lgbouma <bouma.luke@gmail.com>
2024-04-02 12:55:20 -0700

    star_Prot_Teff: working

--- drivers/plot_star_Prot_Teff.py
+++ drivers/plot_star_Prot_Teff.py
@@ -7,5 +7,9 @@ if not os.path.exists(outdir): os.mkdir(outdir)
 
 gp.plot_star_Prot_Teff(outdir, 'teff_age_prot_seed42_nstar20000')
 
-for sampleid in ['Santos19_Santos21_all', 'Santos19_Santos21_clean0']:
+for sampleid in [
+    'Santos19_Santos21_dquality',
+    'Santos19_Santos21_all',
+    'Santos19_Santos21_clean0'
+]:
     gp.plot_star_Prot_Teff(outdir, sampleid)
