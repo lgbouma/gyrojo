7b080617b72f19d4d494f8a46814f89d6cd7ae38
lgbouma <bouma.luke@gmail.com>
2024-04-02 15:06:48 -0700

    update ms

--- paper/collect.sh
+++ paper/collect.sh
@@ -5,3 +5,6 @@ cp $r/star_Prot_Teff/prot_teff_Santos19_Santos21_dquality.pdf .
 
 # plot_koi_mean_prot_teff
 cp $r/koi_mean_prot_teff/koi_mean_prot_teff_koi_X_S19S21dquality_keepgrazing.pdf .
+
+# plot_hist_field_gyro_ages 
+cp $r/hist_field_gyro_ages_20230529/hist_samples_koi_gyro_ages_hist_field_gyro_ages_20230529_maxage3200.pdf .
