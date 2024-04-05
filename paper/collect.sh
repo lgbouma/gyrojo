7dede95c5d4f2c2c823415133c7ea373668516e3
lgbouma <bouma.luke@gmail.com>
2024-04-05 16:02:52 -0700

    correct missing Santos KOIs

--- paper/collect.sh
+++ paper/collect.sh
@@ -7,4 +7,4 @@ cp $r/star_Prot_Teff/prot_teff_Santos19_Santos21_dquality.pdf .
 cp $r/koi_mean_prot_teff/koi_mean_prot_teff_koi_X_S19S21dquality_keepgrazing.pdf .
 
 # plot_hist_field_gyro_ages 
-cp $r/hist_field_gyro_ages_20230529/hist_samples_koi_gyro_ages_hist_field_gyro_ages_20230529_maxage3200.pdf .
+cp $r/hist_field_gyro_ages_20240405/hist_samples_koi_gyro_ages_hist_field_gyro_ages_20240405_maxage3200.pdf .
