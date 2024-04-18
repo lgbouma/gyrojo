8b943e1c380a2c8ef2df612b52035ac5d6782548
lgbouma <bouma.luke@gmail.com>
2024-04-18 10:07:31 -0700

    quality flags: compute for "all KIC" sample

--- drivers/construct_field_star_gyro_quality_flags.py
+++ drivers/construct_field_star_gyro_quality_flags.py
@@ -26,185 +26,211 @@ from glob import glob
 from gyrojo.paths import DATADIR, RESULTSDIR, LOCALDIR, TABLEDIR
 from numpy import array as nparr
 
-datestr = '20230529'
-datestr = '20240405'
 
-csvpath = join(RESULTSDIR, f"field_gyro_posteriors_{datestr}",
-               f"field_gyro_posteriors_{datestr}_gyro_ages_X_GDR3_S19_S21_B20.csv")
+def build_gyro_quality_flag(sample='gyro', datestr='20240405'):
 
-df = pd.read_csv(
-    csvpath, dtype={'dr3_source_id':str, 'KIC':str, 'kepid':str }
-)
+    if sample == 'gyro':
+        csvpath = join(
+            RESULTSDIR, f"field_gyro_posteriors_{datestr}",
+            f"field_gyro_posteriors_{datestr}_gyro_ages_X_GDR3_S19_S21_B20.csv"
+        )
+    elif sample == 'allKIC':
+        # made by get_cleaned_gaiadr3_X_kepler_supplemented_dataframe
+        csvpath = join(
+            DATADIR, "interim", f"kic_X_dr3_supp.csv"
+        )
 
-# drop 3 stars with nan DR3 source id's
-df = df[~(df.dr3_source_id.astype(str) == 'nan')]
+    df = pd.read_csv(
+        csvpath, dtype={'dr3_source_id':str, 'KIC':str, 'kepid':str }
+    )
+    if sample == 'allKIC':
+        df['KIC'] = df.kepid.astype(str)
 
-df['M_G'] = (
-    df['dr3_phot_g_mean_mag'] + 5*np.log10(df['dr3_parallax']/1e3) + 5
-)
+    # drop 3 stars with nan DR3 source id's
+    df = df[~(df.dr3_source_id.astype(str) == 'nan')]
 
-###########################
-# build the quality flags #
-###########################
+    df['M_G'] = (
+        df['dr3_phot_g_mean_mag'] + 5*np.log10(df['dr3_parallax']/1e3) + 5
+    )
 
-df['flag_dr3_ruwe_outlier'] = df['dr3_ruwe'] > 1.4
+    ###########################
+    # build the quality flags #
+    ###########################
 
-df['flag_kepler_gaia_ang_dist'] = df['dr3_kepler_gaia_ang_dist'] > 3
+    df['flag_dr3_ruwe_outlier'] = df['dr3_ruwe'] > 1.4
 
-df['flag_Teffrange'] = (
-    (df['adopted_Teff'] < 3800)
-    |
-    (df['adopted_Teff'] > 6200)
-)
+    df['flag_kepler_gaia_ang_dist'] = df['dr3_kepler_gaia_ang_dist'] > 3
 
-df['flag_logg'] = df['adopted_logg'] < 4.2
+    df['flag_Teffrange'] = (
+        (df['adopted_Teff'] < 3800)
+        |
+        (df['adopted_Teff'] > 6200)
+    )
 
-df['flag_dr3_M_G'] = df['M_G'] < 3.9
+    df['flag_logg'] = df['adopted_logg'] < 4.2
 
-df['flag_is_CP_CB'] = ~(
-    pd.isnull(df.s21_flag1)
-    &
-    pd.isnull(df.s19_flag1)
-)
+    df['flag_dr3_M_G'] = df['M_G'] < 3.9
 
-df['flag_kepler_gaia_xmambiguous'] = (
-    df.count_n_gaia_nbhr >= 1
-)
+    if sample == 'gyro':
+        df['flag_is_CP_CB'] = ~(
+            pd.isnull(df.s21_flag1)
+            &
+            pd.isnull(df.s19_flag1)
+        )
+    elif sample == 'allKIC':
+        df['flag_is_CP_CB'] = np.zeros(len(df)).astype(bool)
 
-#################################
-# check if in Kepler EB catalog #
-#################################
+    df['flag_kepler_gaia_xmambiguous'] = (
+        df.count_n_gaia_nbhr >= 1
+    )
 
-from astropy.io import fits
-from astropy.table import Table
+    #################################
+    # check if in Kepler EB catalog #
+    #################################
 
-fitspath = join(DATADIR, 'literature', 'Kirk_2016_KEBC_2876_rows.fits')
-hdul = fits.open(fitspath)
-kebc_df = Table(hdul[1].data).to_pandas()
-kebc_df['KIC'] = kebc_df['KIC'].astype(str)
-df['KIC'] = df['KIC'].astype(str)
+    from astropy.io import fits
+    from astropy.table import Table
 
-df['flag_in_KEBC'] = (
-    df.KIC.isin(kebc_df.KIC)
-)
+    fitspath = join(DATADIR, 'literature', 'Kirk_2016_KEBC_2876_rows.fits')
+    hdul = fits.open(fitspath)
+    kebc_df = Table(hdul[1].data).to_pandas()
+    kebc_df['KIC'] = kebc_df['KIC'].astype(str)
+    df['KIC'] = df['KIC'].astype(str)
+
+    df['flag_in_KEBC'] = (
+        df.KIC.isin(kebc_df.KIC)
+    )
 
 
-###################################################
-# get the nonsingle star flag via Gaia DR3 xmatch #
-###################################################
-from cdips.utils.gaiaqueries import given_source_ids_get_gaia_data
-source_ids = np.array(df.dr3_source_id).astype(np.int64)
-assert pd.isnull(source_ids).sum() == 0
-groupname = f'field_gyro_{datestr}'
+    ###################################################
+    # get the nonsingle star flag via Gaia DR3 xmatch #
+    ###################################################
+    from cdips.utils.gaiaqueries import given_source_ids_get_gaia_data
+    source_ids = np.array(df.dr3_source_id).astype(np.int64)
+    assert pd.isnull(source_ids).sum() == 0
+    groupname = f'field_{sample}_{datestr}'
 
-gdf = given_source_ids_get_gaia_data(
-    source_ids, groupname, n_max=int(6e4), overwrite=False,
-    which_columns='g.source_id, g.radial_velocity_error, g.non_single_star',
-    gaia_datarelease='gaiadr3'
-)
+    n_max = int(6e4)
+    if sample == 'allKIC':
+        n_max = int(2e5)
 
-# 0: "single".
-# 1,2,3:
-#    • bit 1 (least-significant bit) is set to 1 in case of an astrometric binary
-#    • bit 2 is set to 1 in case of a spectroscopic binary
-#    • bit 3 is set to 1 in case of an eclisping binary
-# Counter({0: 52914, 1: 974, 2: 151, 3: 109})
-df['dr3_non_single_star'] = np.array(gdf.non_single_star)
-df['flag_dr3_non_single_star'] = (df.dr3_non_single_star > 0)
+    gdf = given_source_ids_get_gaia_data(
+        source_ids, groupname, n_max=n_max, overwrite=False,
+        which_columns='g.source_id, g.radial_velocity_error, g.non_single_star',
+        gaia_datarelease='gaiadr3'
+    )
 
-############################################################
-# get the neighbor count via Gaia DR3 source catalog query #
-############################################################
-from cdips.utils.gaiaqueries import given_source_ids_get_neighbor_counts
-dGmag = 2.5
-sep_arcsec = 4
-runid = f'field_gyro_{datestr}_neighbors'
-n_max = 60000
-count_df, ndf = given_source_ids_get_neighbor_counts(
-    source_ids, dGmag, sep_arcsec, runid, n_max=n_max, overwrite=False,
-    enforce_all_sourceids_viable=True, gaia_datarelease='gaiadr3'
-)
+    # 0: "single".
+    # 1,2,3:
+    #    • bit 1 (least-significant bit) is set to 1 in case of an astrometric binary
+    #    • bit 2 is set to 1 in case of a spectroscopic binary
+    #    • bit 3 is set to 1 in case of an eclisping binary
+    # Counter({0: 52914, 1: 974, 2: 151, 3: 109})
+    df['dr3_non_single_star'] = np.array(gdf.non_single_star)
+    df['flag_dr3_non_single_star'] = (df.dr3_non_single_star > 0)
 
-# NOTE this "nbhr_count" number may double-count at times, based on 
-# Kepler-1975 (KOI 7913) == KIC 8873450...  nonetheless, seems to be a specific
-# glitch.
-df['nbhr_count'] = np.array(count_df.nbhr_count)
-df['flag_dr3_crowding'] = df['nbhr_count'] >= 1
+    ############################################################
+    # get the neighbor count via Gaia DR3 source catalog query #
+    ############################################################
+    from cdips.utils.gaiaqueries import given_source_ids_get_neighbor_counts
+    dGmag = 2.5
+    sep_arcsec = 4
+    runid = f'field_{sample}_{datestr}_neighbors'
+    count_df, ndf = given_source_ids_get_neighbor_counts(
+        source_ids, dGmag, sep_arcsec, runid, n_max=n_max, overwrite=False,
+        enforce_all_sourceids_viable=True, gaia_datarelease='gaiadr3'
+    )
 
-#####################
-# RELATIVE AGE FLAG #
-#####################
-from gyrojo.locus_definer import constrained_polynomial_function
-csvpath = join(DATADIR, "interim", "logg_teff_locus_coeffs.csv")
-coeffs = pd.read_csv(csvpath).values.flatten()
-logg_locus = constrained_polynomial_function(nparr(df['adopted_Teff']), coeffs)
-df['flag_farfrommainsequence'] = df['adopted_logg'] < logg_locus
+    # NOTE this "nbhr_count" number may double-count at times, based on 
+    # Kepler-1975 (KOI 7913) == KIC 8873450...  nonetheless, seems to be a specific
+    # glitch.
+    df['nbhr_count'] = np.array(count_df.nbhr_count)
+    df['flag_dr3_crowding'] = df['nbhr_count'] >= 1
 
-# DEPRECATED:
-df['b20t2_rel_E_Age'] = np.abs(df['b20t2_E_Age'])/df['b20t2_Age']
-df['b20t2_rel_e_Age'] = np.abs(df['b20t2_e_Age'])/df['b20t2_Age']
-# df['b20t2_max_rel_Age'] = np.maximum(df['b20t2_rel_E_Age'], df['b20t2_rel_e_Age'])
-# df['flag_b20t2_rel_E_Age'] = df['b20t2_rel_E_Age'] < 0.4
+    #####################
+    # RELATIVE AGE FLAG #
+    #####################
+    from gyrojo.locus_definer import constrained_polynomial_function
+    csvpath = join(DATADIR, "interim", "logg_teff_locus_coeffs.csv")
+    coeffs = pd.read_csv(csvpath).values.flatten()
+    logg_locus = constrained_polynomial_function(nparr(df['adopted_Teff']), coeffs)
+    df['flag_farfrommainsequence'] = df['adopted_logg'] < logg_locus
 
-#    DEPRECATED::
-#    #############
-#    # CAMD flag #
-#    #############
-#    
-#    #TODO: need CAMD flag constructed via Green19 map
-#    # this is a hacky selection in M_G vs (BP-RP), no extinction.  made in
-#    # "session_{datestr}_phot_single_selection_and_other_fun_subsets.glu"
-#    # and 20240405 is just a copy of 20230529
-#    
-#    manual_path = join(
-#        RESULTSDIR,
-#        "glue_interactive_viz",
-#        f"field_gyro_posteriors_{datestr}_GDR3_S19_S21_B20_phot_single.csv"
-#    )
-#    manual_phot_single_df = pd.read_csv(manual_path, dtype={'KIC':str})
-#    
-#    df['KIC'] = df.KIC.astype(str)
-#    
-#    df['flag_camd_outlier'] = ~(df.KIC.isin(manual_phot_single_df.KIC))
+    df['b20t2_rel_E_Age'] = np.abs(df['b20t2_E_Age'])/df['b20t2_Age']
+    df['b20t2_rel_e_Age'] = np.abs(df['b20t2_e_Age'])/df['b20t2_Age']
+    # DEPRECATED:
+    # df['b20t2_max_rel_Age'] = np.maximum(df['b20t2_rel_E_Age'], df['b20t2_rel_e_Age'])
+    # df['flag_b20t2_rel_E_Age'] = df['b20t2_rel_E_Age'] < 0.4
 
-################################
-# finally, is gyro applicable? #
-################################
+    #    DEPRECATED::
+    #    #############
+    #    # CAMD flag #
+    #    #############
+    #    
+    #    #TODO: need CAMD flag constructed via Green19 map
+    #    # this is a hacky selection in M_G vs (BP-RP), no extinction.  made in
+    #    # "session_{datestr}_phot_single_selection_and_other_fun_subsets.glu"
+    #    # and 20240405 is just a copy of 20230529
+    #    
+    #    manual_path = join(
+    #        RESULTSDIR,
+    #        "glue_interactive_viz",
+    #        f"field_gyro_posteriors_{datestr}_GDR3_S19_S21_B20_phot_single.csv"
+    #    )
+    #    manual_phot_single_df = pd.read_csv(manual_path, dtype={'KIC':str})
+    #    
+    #    df['KIC'] = df.KIC.astype(str)
+    #    
+    #    df['flag_camd_outlier'] = ~(df.KIC.isin(manual_phot_single_df.KIC))
 
-# see description of flags in docstring
-flag_bits = {
-    'flag_Teffrange': 0,
-    'flag_logg': 1,
-    'flag_dr3_M_G': 2,
-    'flag_in_KEBC': 3,
-    'flag_kepler_gaia_ang_dist': 4,
-    'flag_kepler_gaia_xmambiguous': 5,
-    'flag_dr3_non_single_star': 6,
-    'flag_dr3_ruwe_outlier': 7,
-    'flag_dr3_crowding': 8,
-    'flag_farfrommainsequence': 9,
-    'flag_is_CP_CB': 10
-}
+    ################################
+    # finally, is gyro applicable? #
+    ################################
 
-# Iterate over the flag columns and update the flag_gyro_quality column
-df['flag_gyro_quality'] = 0
-for flag, bit_pos in flag_bits.items():
-    # Convert the flag column to NumPy array and perform left-shift operation
-    shifted_values = np.left_shift(df[flag].astype(int).values, bit_pos)
-    # Update the 'flag_gyro_quality' column using the shifted values
-    df['flag_gyro_quality'] |= shifted_values
+    # see description of flags in docstring
+    flag_bits = {
+        'flag_Teffrange': 0,
+        'flag_logg': 1,
+        'flag_dr3_M_G': 2,
+        'flag_in_KEBC': 3,
+        'flag_kepler_gaia_ang_dist': 4,
+        'flag_kepler_gaia_xmambiguous': 5,
+        'flag_dr3_non_single_star': 6,
+        'flag_dr3_ruwe_outlier': 7,
+        'flag_dr3_crowding': 8,
+        'flag_farfrommainsequence': 9,
+        'flag_is_CP_CB': 10
+    }
 
-# Define the mask to check bits 0 through 9 inclusive
-mask = 0b0000001111111111
+    # Iterate over the flag columns and update the flag_gyro_quality column
+    df['flag_gyro_quality'] = 0
+    for flag, bit_pos in flag_bits.items():
+        # Convert the flag column to NumPy array and perform left-shift operation
+        shifted_values = np.left_shift(df[flag].astype(int).values, bit_pos)
+        # Update the 'flag_gyro_quality' column using the shifted values
+        df['flag_gyro_quality'] |= shifted_values
 
-# Create the 'flag_is_gyro_applicable' column - uses bits 0 to 9, but ignores
-# bit 10.
-df['flag_is_gyro_applicable'] = ((df['flag_gyro_quality'] & mask) == 0)
+    # Define the mask to check bits 0 through 9 inclusive
+    mask = 0b0000001111111111
 
-outcsv = join(
-    TABLEDIR,
-    f"field_gyro_posteriors_{datestr}_gyro_ages_X_GDR3_S19_S21_B20_with_qualityflags.csv"
-)
+    # Create the 'flag_is_gyro_applicable' column - uses bits 0 to 9, but ignores
+    # bit 10.
+    df['flag_is_gyro_applicable'] = ((df['flag_gyro_quality'] & mask) == 0)
 
-df.to_csv(outcsv, index=False)
-print(f"Wrote {outcsv}")
+    if sample == 'gyro':
+        outcsv = join(
+            TABLEDIR,
+            f"field_gyro_posteriors_{datestr}_gyro_ages_X_GDR3_S19_S21_B20_with_qualityflags.csv"
+        )
+    elif sample == 'allKIC':
+        outcsv = join(
+            TABLEDIR,
+            f"{sample}_{datestr}_X_GDR3_B20_with_qualityflags.csv"
+        )
+
+    df.to_csv(outcsv, index=False)
+    print(f"Wrote {outcsv}")
+
+if __name__ == "__main__":
+    build_gyro_quality_flag(sample='gyro', datestr='20240405')  # ie with rotation
+    build_gyro_quality_flag(sample='allKIC', datestr='20240405')
