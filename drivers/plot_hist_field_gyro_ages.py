3c0cc32c2d98f0802c699f6c7e4c9e0e0b24af4c
lgbouma <bouma.luke@gmail.com>
2024-05-06 09:52:37 -0700

    plot hist field: smarter defaults

--- drivers/plot_hist_field_gyro_ages.py
+++ drivers/plot_hist_field_gyro_ages.py
@@ -8,8 +8,18 @@ cache_id = "hist_field_gyro_ages_20240430"
 outdir = os.path.join(RESULTSDIR, cache_id)
 if not os.path.exists(outdir): os.mkdir(outdir)
 
-maxages = [3200, 4000]
-require_santosonly = [True, False]
+
+# 3200 also implemented
+maxages = [4000]
+
+#no point having True; only effect is it biases against young KOIs
+require_santosonly = [False]
+
+for maxage in maxages:
+    ap.plot_hist_field_gyro_ages(
+        outdir, cache_id, MAXAGE=maxage, s19s21only=0, preciseagesonly=1
+    )
+
 for maxage in maxages:
     for s19s21only in require_santosonly:
         ap.plot_hist_field_gyro_ages(
