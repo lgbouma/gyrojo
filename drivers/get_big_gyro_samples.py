f4bdb2b48a33fe5522e1a193de212272ce92f073
lgbouma <bouma.luke@gmail.com>
2024-05-23 15:54:38 -0700

    add big sample-getter driver

---
+++ drivers/get_big_gyro_samples.py
@@ -0,0 +1,37 @@
+"""
+Draw samples from the gyro age posterior PDFs.
+"""
+from gyrointerp.helpers import sample_ages_from_pdf
+from os.path import join
+import os
+import pandas as pd, numpy as np
+from numpy import array as nparr
+from glob import glob
+
+postdir = '/Users/luke/.gyrointerp_cache/field_gyro_posteriors_20240521/'
+outdir = '/Users/luke/.gyrointerp_cache/bigsamples_field_gyro_posteriors_20240521/'
+if not os.path.exists(outdir): os.mkdir(outdir)
+
+csvpaths = glob(join(postdir, "*_posterior.csv"))
+
+for ix, csvpath in enumerate(csvpaths):
+
+    outcsvname = os.path.basename(csvpath).replace("_posterior", "_bigsamples")
+    outcsvpath = join(outdir, outcsvname)
+    if os.path.exists(outcsvpath):
+        continue
+
+    df = pd.read_csv(csvpath)
+
+    age_pdf = nparr(df.age_post)
+
+    if not np.all(np.isfinite(age_pdf)):
+        #print(f'{ix}/{len(csvpaths)}:  Found nans; skip.')
+        continue
+
+    age_samples = sample_ages_from_pdf(nparr(df.age_grid), age_pdf)
+    outdf = pd.DataFrame({'t_gyro': age_samples})
+
+    outdf.to_csv(outcsvpath, index=False, float_format='%.1f')
+    if ix % 100 == 0:
+        print(f'{ix}/{len(csvpaths)}:  Wrote {outcsvpath}')
