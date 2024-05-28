70f73ad3cb2fe1a9b68a9dff4c9a02c34eee4ff1
lgbouma <bouma.luke@gmail.com>
2024-05-28 11:14:15 -0700

    minor

--- drivers/plot_st_params.py
+++ drivers/plot_st_params.py
@@ -5,14 +5,16 @@ import gyrojo.plotting as ap
 outdir = os.path.join(RESULTSDIR, 'st_params')
 if not os.path.exists(outdir): os.mkdir(outdir)
 
-# logg vs Teff
+# in ms
+ap.plot_st_params(outdir, xkey='dr3_bp_rp', ykey='M_G')
 ap.plot_st_params(outdir, xkey='adopted_Teff', ykey='adopted_logg')
+
+# logg vs Teff
 for vtangcut in ['thindisk', 'thickdisk', 'halo']:
     ap.plot_st_params(outdir, xkey='adopted_Teff', ykey='adopted_logg',
                       vtangcut=vtangcut)
 
 # CAMDs
-ap.plot_st_params(outdir, xkey='dr3_bp_rp', ykey='M_G')
 ap.plot_st_params(outdir, xkey='adopted_Teff', ykey='M_G')
 
 # mKep vs Teff
