2feff42d176f38d79feed585e5d423976e7c5d51
lgbouma <bouma.luke@gmail.com>
2024-06-17 09:28:01 -0700

    McQ14 prep drivers

--- drivers/plot_field_gyro_posteriors.py
+++ drivers/plot_field_gyro_posteriors.py
@@ -7,6 +7,9 @@ from gyrojo.paths import RESULTSDIR
 sampleid = 'Santos19_Santos21_litsupp_all'
 cache_id = "field_gyro_posteriors_20240530"
 
+sampleid = 'McQuillan2014only' # McQuillan2014 only
+cache_id = 'field_gyro_posteriors_McQ14_20240613'
+
 outdir = os.path.join(RESULTSDIR, cache_id)
 if not os.path.exists(outdir): os.mkdir(outdir)
 
