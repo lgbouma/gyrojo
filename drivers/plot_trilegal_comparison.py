35f9cd87b72cda0c6adfb002d99a9129a24d4e4c
lgbouma <bouma.luke@gmail.com>
2024-08-29 10:40:49 -0700

    update drivers/plot_trilegal_comparison.py

--- drivers/plot_trilegal_comparison.py
+++ drivers/plot_trilegal_comparison.py
@@ -5,4 +5,7 @@ import gyrojo.plotting as ap
 outdir = os.path.join(RESULTSDIR, 'trilegal_comparison')
 if not os.path.exists(outdir): os.mkdir(outdir)
 
-ap.plot_trilegal_comparison(outdir)
+ap.plot_threepanel_trilegal_comparison(outdir)
+
+ap.plot_trilegal_comparison(outdir, const_sfr=0)
+ap.plot_trilegal_comparison(outdir, const_sfr=1)
